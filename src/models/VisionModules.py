f15c91fd2a3891498cf6eba121c1b314f7d7a044
Tianchen Wang <tw17@illinois.edu>
2023-11-05 01:05:23 -0600

    update vision transformer

--- src/models/VisionModules.py
+++ src/models/VisionModules.py
@@ -1,16 +1,28 @@
 import torch
 import torch.nn as nn
-from timm.models import VisionTransformer
+
+from transformers import ViTModel, ViTImageProcessor
 
 class VisionEncoder(nn.Module):
     def __init__(self, args):
         super(VisionEncoder, self).__init__()
 
         self.args = args
-        self.config = args.dataset_config["VisionEncoder"]
+        # self.config = args.dataset_config["VisionEncoder"]
         
-        # self.model = VisionTransformer()
+        self.image_processor = ViTImageProcessor.from_pretrained("google/vit-base-patch16-224-in21k")
+        self.vision_model = ViTModel.from_pretrained("google/vit-base-patch16-224-in21k")
 
-    def forward(self):        
-        raise NotImplementedError("Vision Encoder forward pass not implemented yet.")
-        pass
+    def forward(self, panels):
+        '''
+            panels: (batch_size, seq_length, 3, 256, 256)
+        '''
+        merged_batch = panels.view(-1, 3, 256, 256)
+        
+        inputs = self.image_processor(merged_batch, return_tensors="pt")
+        outputs = self.vision_model(**inputs)
+        
+        embeddings = outputs.last_hidden_state
+        cls_embeddings = embeddings[:, 0, :]
+        
+        return cls_embeddings.view(panels.shape[0], panels.shape[1], -1)
