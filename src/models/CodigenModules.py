1af3ea2f6899dbcff610964749af454efedde582
Tomoyoshi Kimura <67913271+tomoyoshki@users.noreply.github.com>
2023-11-07 22:00:36 -0600

    Update CodigenModules.py

--- src/models/CodigenModules.py
+++ src/models/CodigenModules.py
@@ -1,5 +1,5 @@
 import torch.nn as nn
-
+from models.DecoderModules import Decoder
 
 class Codigen(nn.Module):
     def __init__(self, args):
@@ -16,17 +16,31 @@ class Codigen(nn.Module):
         }
     
         
-        self.sequential_network = None
-
-        self.decoder = None
+        self.sequential_network = nn.Linear(visual_emb_dim + lang_emb_dim + seq_dim, seq_dim)
+        self.decoder = Decoder(self.args, self.tokenizer)
 
     def forward_encoder(self, panels, texts):
+
+        """
+        V (b, seq_len, 3, dim, dim) -> 
+
+
+
+        RNN = RNN(RNN_output, token)
+        previous_input = (V + L + previous_input) fusion(Concat, Transformer (Attention) Fusion, Linear Layer, MLP, LSTM, RNN)
+        L
+
+        V P  
+        L
+        """
         
         # encoder
         sequential_embedding = None
         for i in range(0, 3):
-            panel_embedding = self.encoder["image"](panels[i])
-            text_embedding = self.encoder["texts"](texts[i])
+            panel_embedding = self.vision_encoder(panels[i]) # V (v, 1, 3, image_dim, image_dim) -> (b, visual_emb_dim)
+            text_embedding = self.language_encoder(texts[i]) # L (b, token_len, emb_dim) -> pool -> (b, lang_emb_dim)
+            # check VisBaseline stuff
+            
 
             sequential_embedding = self.sequential_network[i](panel_embedding, text_embedding, sequential_embedding)
         
@@ -35,9 +49,9 @@ class Codigen(nn.Module):
         
         return fused_embeddings
 
-    def forward_decoder(self, embeddings):
-        decoded_text = self.decoder(embeddings)
-        return decoded_text
+    def forward_decoder(self, embeddings, gt_token_id=None):
+        token, text = self.decoder(embeddings, gt_token_id)
+        return token, text
     
     def forward(self, panels=None, text=None, embeddings=None):
         # raise NotImplementedError("Forward pass not implemented yet.")
